b1798fba91ec38ee5cb4aaf0d1dedea803816ae7
vy-shoe <vadimulam6@gatech.edu>
2022-01-03 22:53:23 -0500

    search text field UI implemented

--- Clima/Controller/WeatherViewController.swift
+++ Clima/Controller/WeatherViewController.swift
@@ -8,17 +8,46 @@
 
 import UIKit
 
-class WeatherViewController: UIViewController {
+//UI TextField Delegate allows the management of editing and validation of text in a text field object
+class WeatherViewController: UIViewController, UITextFieldDelegate {
 
     @IBOutlet weak var conditionImageView: UIImageView!
     @IBOutlet weak var temperatureLabel: UILabel!
     @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet weak var searchTextField: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        searchTextField.delegate = self //set the delegate as current class... textfield should report the user actions back to the ViewController
     }
 
-
+    @IBAction func searchPressed(_ sender: UIButton) {
+        searchTextField.endEditing(true) //tell searchfield that editing is done and keyboard is dismissed
+    }
+    
+    // User pressed "Return/Go"
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        searchTextField.endEditing(true) //tell searchfield that editing is done and keyboard is dismissed
+        return true
+    }
+    
+    //If user clicks go or search, validation on text will occur
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        if textField.text != "" { //allow textfield to end editing
+            return true
+        } else {
+            textField.placeholder = "Type Something" //remind user to type in search bar
+            return false
+        }
+    }
+    
+    //View Controller can empty textfield once user has returned/ended editing, prevents repetition
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        //Use textfield to get the weather for that city and save before clearing
+        
+        
+        searchTextField.text = "" //removes the text once searched
+    }
+    
 }
 
